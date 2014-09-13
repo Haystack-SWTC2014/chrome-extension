18b6344fb8fab43e08036588e11237792364a60f
MD ROBIUL ISLAM <mdrobiulislam@MDs-MacBook-Pro.local>
2014-09-13 15:28:12 -0500

    initial commit, appending div into google page

---
+++ divinjector.js
@@ -0,0 +1,30 @@
+/**
+
+
+var iframe  = document.createElement ("iframe");
+iframe.src  = chrome.extension.getURL ("main.html");
+iframe.style = "width:600px;border:10px;position:relative;top:0;right:0;";
+iframe.width="300px";
+iframe.height="250px";
+iframe.id="randomid";
+var yourDIV = document.getElementById("gsr");
+yourDIV.appendChild(iframe);
+
+$(function() { //shorthand document.ready function
+	
+
+
+	
+	var some = $('#gbqfq').val();
+
+	
+
+});
+
+ */
+
+var some = $('#gbqfq').val();
+$(function() {
+$( "#gsr" ).append("<div id='haystack' style='' solid #000;\"'>Haystack</div>")
+});
+
