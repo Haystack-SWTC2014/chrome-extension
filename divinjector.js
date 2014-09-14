f010243d5feabd89e31cda0b573d5d32b858c5d5
MD ROBIUL ISLAM <mdrobiulislam@MDs-MacBook-Pro.local>
2014-09-13 20:05:10 -0500

    first working version of the chrome extension

--- divinjector.js
+++ divinjector.js
@@ -1,30 +1,89 @@
 /**
-
-
-var iframe  = document.createElement ("iframe");
-iframe.src  = chrome.extension.getURL ("main.html");
-iframe.style = "width:600px;border:10px;position:relative;top:0;right:0;";
-iframe.width="300px";
-iframe.height="250px";
-iframe.id="randomid";
-var yourDIV = document.getElementById("gsr");
-yourDIV.appendChild(iframe);
-
-$(function() { //shorthand document.ready function
-	
-
-
-	
-	var some = $('#gbqfq').val();
-
-	
-
-});
-
+ * 
+ * 
  */
 
-var some = $('#gbqfq').val();
-$(function() {
-$( "#gsr" ).append("<div id='haystack' style='' solid #000;\"'>Haystack</div>")
-});
 
+var search = $('#gbqfq').val();
+
+$(function() {
+
+	$("#gsr").append("<div id='haystack'></div>");
+
+	$.post("http://localhost/step1.php", {
+		query : search
+	}, function(data, status) {
+
+		var obj = $.parseJSON(data);
+
+		var items = [];
+
+		if (obj.type == "domain") {
+
+			//alert("need step 2");
+			$.each(obj.options, function(key, val) {
+
+				items.push("<a id='hay" + key + "' href='#' ><li>" + val
+						+ "</li></a>");
+
+				// alert(key);
+
+			});
+
+			$("<ul/>", {
+				"class" : "my-new-list",
+				html : items.join("")
+			}).appendTo("div#haystack");
+
+			$.each(obj.options, function(key, val) {
+				$("#hay" + key).on(
+						"click",
+						function() {
+
+							$("div#haystack").html("");
+
+							$.post("http://localhost/step2.php", {
+								query : search,
+								domain : val
+							}, function(data, status) {
+								var obj2 = $.parseJSON(data);
+								//alert(obj2.type);
+
+								var items = [];
+
+								if (obj2.type == "search") {
+
+									$.each(obj2.options, function(key2, val2) {
+										items.push("<a id='hay" + key2
+												+ "' href='#'><li>" + val2 + "</li></a>");
+									});
+
+									$("<ul/>", {
+										"class" : "my-new-list",
+										html : items.join("")
+									}).appendTo("div#haystack");
+									
+									$.each(obj2.options, function(key2, val2) {
+										$("#hay" + key2).on(
+												"click",
+												function() {
+													
+													//alert(val2);
+													$('#gbqfq').val(val2);
+													$( "#gbqf" ).submit();
+												});
+									});
+
+								}
+							});
+
+						});
+			});
+
+		} else {
+
+		}
+
+	});
+
+});
